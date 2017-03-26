3b35b04bac1dea2cfc956be2a1350dbc414d0126
Neo Dev <neodev31@gmail.com>
2017-03-26 03:38:56 -0400

    added java solution project

---
+++ JavaedX-HTWCC/src/week2/Week2Stack1.java
@@ -0,0 +1,87 @@
+package week2;
+
+import java.io.*;
+import java.util.*;
+
+public class Week2Stack1 {
+
+	static class FastScanner {
+		static BufferedReader br;
+		static StringTokenizer st;
+
+		FastScanner(File f) {
+			try {
+				br = new BufferedReader(new FileReader(f));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public FastScanner(InputStream f) {
+			br = new BufferedReader(new InputStreamReader(f));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+	}
+
+	static FastScanner newInput() throws IOException {
+		return new FastScanner(new File("input.txt"));
+	}
+
+	static PrintWriter newOutput() throws IOException {
+		return new PrintWriter("output.txt");
+	}
+
+	public static void main(String[] args) throws IOException {
+		try (PrintWriter out = newOutput()) {
+			FastScanner in = newInput();
+			
+			Stack<Integer> ks = new Stack<Integer>();
+			//LinkedList<Integer> list = new LinkedList<Integer>();
+			
+			int number_of_operation = in.nextInt();
+			int i = 0;
+			
+			while(i<number_of_operation){
+				
+				if("+".equals(in.next())){
+					ks.push(in.nextInt());
+				}else{
+					out.println(ks.peek());
+					//list.add(ks.peek());
+					ks.pop();
+				}
+				
+				i++;
+			}
+			
+//			int j = 0;
+//			while(j < list.size()){
+//				out.println(list.get(j));
+//				j++;
+//			}
+		}
+	}
+
+}
