498492cebdea103ce54312f41e30ff2ee02fe7f6
Neo Dev <neodev31@gmail.com>
2017-03-30 02:13:00 -0400

    updated week 2 prob 4 soln

---
+++ JavaedX-HTWCC/src/week2/Week2BracketSequence4.java
@@ -0,0 +1,100 @@
+package week2;
+
+import java.io.*;
+import java.util.*;
+
+public class Week2BracketSequence4 {
+
+	static class FastScanner {
+		static BufferedReader br;
+		static StringTokenizer st;
+
+		FastScanner(File f) {
+			try {
+				br = new BufferedReader(new FileReader(f));
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public FastScanner(InputStream f) {
+			br = new BufferedReader(new InputStreamReader(f));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+	}
+
+	static FastScanner newInput() throws IOException {
+		return new FastScanner(new File("input.txt"));
+	}
+
+	static PrintWriter newOutput() throws IOException {
+		return new PrintWriter("output.txt");
+	}
+	
+	public static String checkSequence(String str){
+		String priv = "";
+		
+		while (str.length() !=  priv.length()){
+			priv = str;
+			str = str.replace("()", "");
+			str = str.replace("[]", "");
+		}
+		
+		if(str.length() == 0)
+			return "YES";
+		else
+			return "NO";
+		
+	}
+
+	public static void main(String[] args) throws IOException {
+		try (PrintWriter out = newOutput()) {
+			FastScanner in = newInput();
+			
+			//Stack<Integer> ks = new Stack<Integer>();
+			LinkedList<Integer> list = new LinkedList<Integer>();
+			
+			int number_of_operation = in.nextInt();
+			int i = 0;
+			
+			while( i < number_of_operation ){
+				
+				String str = in.next();
+				
+				out.println(checkSequence(str));
+				//System.out.println(str);
+				
+				i++;
+			}
+			
+//			int j = 0;
+//			while(j < list.size()){
+//				out.println(list.get(j));
+//				j++;
+//			}
+		}
+	}
+
+}
